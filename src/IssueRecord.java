2c8dc470098f21976697633baa8f412595372935
MahfuzKabirJoy <mahfuzkabirjoy@gmail.com>
2026-09-25 01:04:26 +0600

    Add IssueRecord class for book issue management

---
+++ src/IssueRecord.java
@@ -0,0 +1,36 @@
+import java.time.LocalDate;
+
+public class IssueRecord {
+
+    private Book book;
+    private String memberName;
+    private LocalDate issueDate;
+    private LocalDate returnDate;
+
+    public IssueRecord(Book book, String memberName) {
+        this.book = book;
+        this.memberName = memberName;
+        this.issueDate = LocalDate.now();
+        this.returnDate = null;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void returnBook() {
+        returnDate = LocalDate.now();
+    }
+}
