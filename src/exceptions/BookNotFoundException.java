688ffc069c9862dde103b457391dcd8272fd2896
Mahabubul Alam Pritom <pritomhamilton787@gmail.com>
2026-09-25 19:21:56 +0600

    Add BookNotFoundException for library management

--- src/exceptions/BookNotFoundException.java
+++ src/exceptions/BookNotFoundException.java
@@ -1,1 +1,8 @@
+package librarymanagement.exceptions;
 
+public class BookNotFoundException extends Exception {
+
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+}
