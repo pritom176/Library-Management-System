9189d900275f921ccfc871987b29c8d46d29c295
Mahabubul Alam Pritom <pritomhamilton787@gmail.com>
2026-09-25 19:23:02 +0600

    Add BookUnavailableException class

--- src/exceptions/BookUnavailableException.java
+++ src/exceptions/BookUnavailableException.java
@@ -1,1 +1,8 @@
+package librarymanagement.exceptions;
 
+public class BookUnavailableException extends Exception {
+
+    public BookUnavailableException(String message) {
+        super(message);
+    }
+}
